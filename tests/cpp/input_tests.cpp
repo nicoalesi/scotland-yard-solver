1a5401abd231cfbf284202d920253e9b89c2446d
nicoalesi <nicoalesi.cs@gmail.com>
2025-09-13 01:28:05 +0200

    new(test): add unit tests for input api
    Add more unit tests for input API.

--- tests/cpp/input_tests.cpp
+++ tests/cpp/input_tests.cpp
@@ -14,10 +14,13 @@ std::string create_file(const std::string& content) {
 
 // Helper: delete a file
 void delete_file(const std::string& filename) {
-    std::remove(filename.c_str());
+    if (std::remove(filename.c_str()) != 0) {
+        throw std::runtime_error("Failed to delete file '" + filename + "'.");
+    }
 }
 
 
+// No error
 TEST(read_ints, ReadCorrectly) {
     std::string file = create_file("10\n20\n30\n");
 
@@ -78,6 +81,117 @@ TEST(read_ints, ThrowIfNotEnoughLines) {
     } catch (const std::runtime_error& e) {
         std::string msg = e.what();
         EXPECT_TRUE(msg.find("Expected 3 lines but got 2") != std::string::npos);
+    }
+
+    delete_file(file);
+}
+
+
+// No error
+TEST(read_int_pairs, ReadCorrectly) {
+    std::string file = create_file(
+        "1 2\n"
+        "3 4\n"
+        "5 6\n"
+    );
+
+    std::vector<std::pair<int, int>> result;
+    ASSERT_NO_THROW(read_int_pairs(result, file, 3));
+    EXPECT_EQ(result.size(), 3);
+    EXPECT_EQ(result[0], std::make_pair(1, 2));
+    EXPECT_EQ(result[1], std::make_pair(3, 4));
+    EXPECT_EQ(result[2], std::make_pair(5, 6));
+
+    delete_file(file);
+}
+
+
+// Error if vector not empty
+TEST(read_int_pairs, ThrowIfVectorNotEmpty) {
+    std::vector<std::pair<int, int>> result = {{1, 2}, {3, 4}};
+    EXPECT_THROW(read_int_pairs(result, "anyfile.txt", 1), std::invalid_argument);
+}
+
+
+// Error if file does not exist
+TEST(read_int_pairs, ThrowIfFileMissing) {
+    std::vector<std::pair<int, int>> result;
+    EXPECT_THROW(read_int_pairs(result, "nonexistent_file.txt", 1), std::runtime_error);
+}
+
+
+// Error if invalid integer pair in file
+TEST(read_int_pairs, ThrowIfInvalidIntegerPair) {
+    std::string file = create_file(
+        "10 11\n"
+        "abc\n"
+        "30 21\n"
+    );
+
+    std::vector<std::pair<int, int>> result;
+    EXPECT_THROW(read_int_pairs(result, file, 3), std::runtime_error);
+    result.clear();
+
+    try {   
+        read_int_pairs(result, file, 3);
+    } catch (const std::runtime_error& e) {
+        std::string msg = e.what();
+        EXPECT_TRUE(msg.find("Invalid integer pair on line 2") != std::string::npos);
+    }
+
+    delete_file(file);
+    file = create_file(
+        "10 11\n"
+        "12 9\n"
+        "30\n"
+    );
+
+    result.clear();
+    EXPECT_THROW(read_int_pairs(result, file, 3), std::runtime_error);
+    result.clear();
+
+    try {   
+        read_int_pairs(result, file, 3);
+    } catch (const std::runtime_error& e) {
+        std::string msg = e.what();
+        EXPECT_TRUE(msg.find("Invalid integer pair on line 3") != std::string::npos);
+    }
+
+    delete_file(file);
+    file = create_file(
+        "10 \n"
+        "11 12\n"
+        "30\n"
+    );
+
+    result.clear();
+    EXPECT_THROW(read_int_pairs(result, file, 3), std::runtime_error);
+    result.clear();
+    
+    try {   
+        read_int_pairs(result, file, 3);
+    } catch (const std::runtime_error& e) {
+        std::string msg = e.what();
+        EXPECT_TRUE(msg.find("Invalid integer pair on line 1") != std::string::npos);
+    }
+
+    delete_file(file);
+}
+
+
+// Error if file has fewer lines than requested
+TEST(read_int_pairs, ThrowIfNotEnoughLines) {
+    std::string file = create_file("10 9\n");
+
+    std::vector<std::pair<int, int>> result;
+    EXPECT_THROW(read_int_pairs(result, file, 3), std::runtime_error);
+    result.clear();
+
+    try {
+        read_int_pairs(result, file, 3);
+    } catch (const std::runtime_error& e) {
+        std::string msg = e.what();
+        EXPECT_TRUE(msg.find("Expected 3 lines but got 1") != std::string::npos);
     }
 
     delete_file(file);
